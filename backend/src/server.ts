bdf5c01f33a458482272a8d4b1a72046437b9e9d
JaoTz <codgomes@gmail.com>
2022-02-05 17:18:02 -0300

    Adicionando dependencias e Mysql ao Docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,6 @@
-console.log("teste")
+import 'dotenv/config'
+import app from "./app"
+
+app.listen(Number(process.env.PORT), String(process.env.HOST), () => {
+    console.log(`Servidor rodando!`)
+})

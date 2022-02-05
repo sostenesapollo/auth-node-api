ae3ced2f4dc579160aab6ac47ffb8661fad81257
JaoTz <codgomes@gmail.com>
2022-02-05 19:28:19 -0300

    Configurando EsLint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,20 +1,28 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  'env': {
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'quotes': ['warn', 'single'],
+    'quote-props': ['error', 'always'],
+    'indent': ['error', 2],
+    'func-call-spacing': ['error', 'never'],
+    'function-call-argument-newline': ['error', 'never'],
+    'computed-property-spacing': ['error', 'always'],
+    'eol-last': ['error', 'always'],
+    'max-len': ['error', { 'code': 100 }]
+  }
 }
--- backend/src/migrations/20220205203024_createUser.ts
+++ backend/src/migrations/20220205203024_createUser.ts
@@ -1,16 +1,14 @@
-import { Knex } from "knex";
+import { Knex } from 'knex';
 
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.createTable('user', (table) => {
-        table.increments()
-        table.string('name')
-        table.timestamps()
-    })
+  return knex.schema.createTable('user', (table) => {
+    table.uuid('id').primary().notNullable()
+    table.string('username', 25).notNullable()
+    table.string('password', 16).notNullable()
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTableIfExists('user')
+  return knex.schema.dropTableIfExists('user')
 }
-
